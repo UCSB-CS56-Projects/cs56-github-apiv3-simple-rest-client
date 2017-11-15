1c4b12cf8210beef2071f6bd0799bbb1428aa099
Yossi Hertzberg <jhertzberg@umail.ucsb.edu>
2017-11-15 14:51:11 -0800

    split up code in main method to seperate methods, looks cleaner, easier to read

--- src/edu/ucsb/cs56/projects/github/repo_curation/listLegacyCodeRepos.java
+++ src/edu/ucsb/cs56/projects/github/repo_curation/listLegacyCodeRepos.java
@@ -8,15 +8,33 @@ import java.util.Collection;
 
 public class listLegacyCodeRepos{
         public static void main(String[] args) {
-                try{
-                        Collection<GHRepository> lst = GitHub.connect().getUser("UCSB-CS56-Projects").getRepositories().values();
+                
+		printAllRepoNames("UCSB-CS56-Projects");
+        	printNumberOfRepos("UCSB-CS56-Projects");
+	
+	}
+
+	public static void printAllRepoNames(String user){
+		try{
+                        Collection<GHRepository> lst = GitHub.connect().getUser(user).getRepositories().values();
                         for (GHRepository r : lst) {
                                 System.out.println("Name of repo: " + r.getName());
-				
+
                         }
+                       // System.out.println("Number of repos: " + lst.size());
+                }catch(IOException e){
+                        e.printStackTrace();
+                }
+
+	}
+
+	public static void printNumberOfRepos(String user){
+		try{
+                        Collection<GHRepository> lst = GitHub.connect().getUser("UCSB-CS56-Projects").getRepositories().values();
                         System.out.println("Number of repos: " + lst.size());
                 }catch(IOException e){
                         e.printStackTrace();
                 }
-        }
+
+	}
 }                   
